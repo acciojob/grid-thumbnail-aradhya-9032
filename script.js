27a6f95fa1161cbb43aeecf5e48367d4ecd24adf
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2024-01-25 14:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function expandImage(src) {
+  const fullsize = document.getElementById('fullsize');
+  const fullsizeImg = document.getElementById('fullsize-img');
+  fullsizeImg.src = src;
+  fullsize.style.display = 'flex';
+}
+
+function collapseImage() {
+  const fullsize = document.getElementById('fullsize');
+  fullsize.style.display = 'none';
+}
